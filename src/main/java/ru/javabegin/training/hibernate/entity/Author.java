b8190205b2f1e06fe6d26f7cab371787f2611cf3
sergias <sergias@ukr.net>
2020-08-03 18:05:19 +0300

    Mapping

---
+++ src/main/java/ru/javabegin/training/hibernate/entity/Author.java
@@ -0,0 +1,19 @@
+package ru.javabegin.training.hibernate.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+public class Author implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // генерация ID через Autoincrement в MySQL
+	private long id;
+
+	private String name;
+}
---
+++ src/main/java/ru/javabegin/training/hibernate/entity/Book.java
@@ -0,0 +1,13 @@
+package ru.javabegin.training.hibernate.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+public class Book {
+
+}
