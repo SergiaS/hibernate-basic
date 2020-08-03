82075ef661ed26c1ccb9f7ee33f99f5386ec422b
sergias <sergias@ukr.net>
2020-08-03 18:08:23 +0300

    Mapping - HW

--- src/main/java/ru/javabegin/training/hibernate/entity/Book.java
+++ src/main/java/ru/javabegin/training/hibernate/entity/Book.java
@@ -4,10 +4,22 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
 
 @Entity
 @Getter
 @Setter
-public class Book {
+public class Book implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private Long author_id;
+
+	private String name;
 
 }
