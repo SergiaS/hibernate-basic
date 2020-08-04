c08d365a23082534f08e1db106360aed8bc75b9f
sergias <sergias@ukr.net>
2020-08-04 08:00:29 +0300

    Added logging + Grep Console plugin.

--- src/main/java/ru/javabegin/training/hibernate/Start.java
+++ src/main/java/ru/javabegin/training/hibernate/Start.java
@@ -2,14 +2,19 @@ package ru.javabegin.training.hibernate;
 
 import org.hibernate.Session;
 import ru.javabegin.training.hibernate.entity.Author;
-import ru.javabegin.training.hibernate.entity.Book;
+
+import org.jboss.logging.Logger;
 
 public class Start {
+
+	private static final Logger LOG = Logger.getLogger(AuthorHelper.class.getName());
+
 	public static void main(String[] args) {
 		Session session = HibernateUtil.getSessionFactory().openSession();
 
-		for (Book book : new BookHelper().getBookList()) {
-			System.out.println("Book name: " + book.getName());
+		for (Author author : new AuthorHelper().getAuthorList()) {
+//			System.out.println("Book name: " + book.getName());
+			LOG.warn(author.getName());
 		}
 
 		HibernateUtil.getSessionFactory().close(); // закрываем фабрику, иначе программа останется в "зависнутом состоянии"
